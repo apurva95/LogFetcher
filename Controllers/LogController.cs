4f875b927d30b596d7d5aa3a01125ecc0f73f16e
apurva95 <apurvatandon@gmail.com>
2023-06-29 20:12:10 +0100

    Fixes

--- Controllers/LogController.cs
+++ Controllers/LogController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Nest;
+using System.Globalization;
 using System.Text;
 using System.Xml;
 
@@ -94,10 +95,6 @@ namespace LogFetcher.Controllers
                 {
                     var searchResponse = await _elasticClient.SearchAsync<LogMessage>(s => s.Index(uniqueId)
                         .Query(q => q.MultiMatch(x => x.Fields(z => z.Field(e => e.Level).Field(e => e.Message)).Query(searchTerm))
-                        //.Match(m => m
-                        //    .Field(f => f.Level)
-                        //    .Query(searchTerm)
-                        //)
                         ).Highlight(h => h
                 .Fields(f => f
                     .Field(ff => ff.Message).Field(x => x.Level)
@@ -118,37 +115,238 @@ namespace LogFetcher.Controllers
 
         }
 
-        [HttpGet("visualisationLogs")]
-        public async Task<ActionResult<IEnumerable<LogDto>>> GetLogsForVisualize(string uniqueId)
+        [HttpPost("search")]
+        public async Task<IActionResult> SearchLogs([FromBody] LogSearchRequest request)
         {
-            var searchResponse = await _elasticClient.SearchAsync<LogMessage>(s => s
-                 .Index(uniqueId)
+            try
+            {
+                List<LogMessage> logMsgs = new();
+                // Construct the search query based on selected buckets
+                //var query = new BoolQuery();
+
+                ////// Add time range filter
+                //var timeFilter = new BoolQuery();
+                //foreach (var interval in request.SelectedIntervals)
+                //{
+                //    var intervalStart = interval.Split('-')[0].Trim();
+                //    var intervalEnd = interval.Split('-')[1].Trim();
+
+                //    var rangeQuery = new DateRangeQuery
+                //    {
+                //        Field = "TimeStamp",
+                //        GreaterThanOrEqualTo = intervalStart,
+                //        LessThanOrEqualTo = intervalEnd
+                //    };
+
+                //    timeFilter.Should = new QueryContainer[] { rangeQuery };
+                //}
+
+                //// Add level filter
+                //var processedLevels = request.SelectedLevels.Select(l => l.ToLower());
+                //var levelFilter = new TermsQuery
+                //{
+                //    Field = "level",
+                //    Terms = processedLevels // Assuming the selected level doesn't contain additional colons or brackets
+                //};
+
+                //query.Filter = new List<QueryContainer> { timeFilter, levelFilter };
+
+                //// Create the search descriptor
+                //var searchDescriptor = new SearchDescriptor<LogMessage>()
+                //    .Index("deloitte_usi-ui-wage-2c006383-679d-4148-87ce-a6b722c41734") // Replace "your_index_name" with the actual index name
+                //    .Query(q => query);
+                var scrollResponse = _elasticClient.Search<LogMessage>(s => s.Index(request.RegistrationId)
+        .Query(q => q
+            .Terms(t => t
+                .Field(f => f.Level)
+                .Terms(request.SelectedLevels.Select(l => l.ToLower()))
+            )
+        ).Size(2) // Set the initial batch size
+                                    .Scroll("1m") // Set the scroll time window
+    );
+                while (scrollResponse.Documents.Any())
+                {
+                    logMsgs.AddRange(scrollResponse.Documents);
+
+                    // Get the next batch using the scroll ID
+                    scrollResponse = await _elasticClient.ScrollAsync<LogMessage>("1m", scrollResponse.ScrollId);
+                }
+                // Perform the search request
+                //var searchResponse1 = await _elasticClient.SearchAsync<LogMessage>(searchDescriptor);
+
+                // Process the search results and return them
+                //var logs = searchResponse.Hits.Select(hit => hit.Source);
+                return Ok(logMsgs.OrderBy(x => x.TimeStamp).Select(x => new LogMessage { Level = GetType(x.Level), Message = x.Message, TimeStamp = x.TimeStamp }).ToList());
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception here
+            }
+            return Ok();
+        }
+
+        [HttpGet("visualisationLogs")]
+        public async Task<ActionResult<Dictionary<string, int>>> GetLogsForVisualize(string uniqueId)
+        {
+            List<LogMessage> logMsgs = new();
+            var scrollResponse = await _elasticClient.SearchAsync<LogMessage>(s => s
+                 .Index(uniqueId).Size(2) // Set the initial batch size
+                                    .Scroll("1m") // Set the scroll time window
              );
 
-            var logMessages = searchResponse.Documents.OrderBy(x => x.TimeStamp).ToList();
-            var logs = new List<LogDto>();
+            while (scrollResponse.Documents.Any())
+            {
+                logMsgs.AddRange(scrollResponse.Documents);
 
+                // Get the next batch using the scroll ID
+                scrollResponse = await _elasticClient.ScrollAsync<LogMessage>("1m", scrollResponse.ScrollId);
+            }
+
+            var logMessages = logMsgs.OrderBy(x => x.TimeStamp).ToList();
+            var dict = new Dictionary<string, int>();
             foreach (var logMessage in logMessages)
             {
                 var level = ExtractLogLevel(logMessage.Message);
-                var logDto = new LogDto
+                if (!dict.ContainsKey(level))
                 {
-                    Level = level,
-                    Message = logMessage.Message
-                };
-                logs.Add(logDto);
+                    dict.Add(level, 1);
+                }
+                else
+                {
+                    dict[level]++;
+                }
             }
 
-            return Ok(logs);
+            return Ok(dict);
+        }
+
+        [HttpGet("visualisationLogsForLineGraph")]
+        public async Task<ActionResult<Dictionary<string, int>>> GetLogsForVisualizeForLineGraph(string uniqueId)
+        {
+            try
+            {
+                List<LogMessage> logMsgs = new();
+                var scrollResponse = await _elasticClient.SearchAsync<LogMessage>(s => s
+                     .Index(uniqueId).Size(2) // Set the initial batch size
+                                    .Scroll("1m")
+                 );
+
+                while (scrollResponse.Documents.Any())
+                {
+                    logMsgs.AddRange(scrollResponse.Documents);
+
+                    // Get the next batch using the scroll ID
+                    scrollResponse = await _elasticClient.ScrollAsync<LogMessage>("1m", scrollResponse.ScrollId);
+                }
+
+                var logMessages = logMsgs.OrderBy(x => x.TimeStamp).ToList();
+                var dict = new Dictionary<string, int>();
+
+                // Get the timestamp of the first log
+                var firstLogTime = logMessages.First().TimeStamp;
+
+                // Get the timestamp of the most recent log
+                var recentLogTime = logMessages.Last().TimeStamp;
+
+                // Initialize the start time at the first log timestamp
+                var startTime = new DateTime(firstLogTime.Year, firstLogTime.Month, firstLogTime.Day, 0, 0, 0);
+
+                // Iterate until the current time reaches or exceeds the recent log timestamp
+                while (startTime <= recentLogTime)
+                {
+                    // Calculate the end time for the current day
+                    var endTime = startTime.AddDays(1);
+
+                    // Format the day range as "YYYY-MM-dd"
+                    var dayRange = startTime.ToString("dd-MM-yyyy");
+
+                    // Count the number of logs within the day range
+                    var logCount = logMessages.Count(logMessage => logMessage.TimeStamp >= startTime && logMessage.TimeStamp < endTime);
+
+                    // Add the day range and log count to the dictionary
+                    dict.Add(dayRange, logCount);
+
+                    // Update the start time for the next iteration
+                    startTime = endTime;
+                }
+
+                return Ok(dict);
+            }
+            catch(Exception e)
+            {
+
+            }
+            return Ok();
+        }
+
+        [HttpGet("visualisationLogsForLineGraphError")]
+        public async Task<ActionResult<Dictionary<string, int>>> GetLogsForVisualizeForLineGraphError(string uniqueId)
+        {
+            try
+            {
+                List<LogMessage> logMsgs = new();
+                var scrollResponse = await _elasticClient.SearchAsync<LogMessage>(s => s
+                    .Index(uniqueId)
+                    .Size(2) // Set the initial batch size
+                    .Scroll("1m")
+                );
+
+                while (scrollResponse.Documents.Any())
+                {
+                    logMsgs.AddRange(scrollResponse.Documents);
+
+                    // Get the next batch using the scroll ID
+                    scrollResponse = await _elasticClient.ScrollAsync<LogMessage>("1m", scrollResponse.ScrollId);
+                }
+
+                var logMessages = logMsgs.OrderBy(x => x.TimeStamp).ToList();
+                var dict = new Dictionary<string, int>();
+
+                // Get the timestamp of the first log
+                var firstLogTime = logMessages.First().TimeStamp;
+
+                // Get the timestamp of the most recent log
+                var recentLogTime = logMessages.Last().TimeStamp;
+
+                // Initialize the start time at the first log timestamp
+                var startTime = new DateTime(firstLogTime.Year, firstLogTime.Month, firstLogTime.Day, 0, 0, 0);
+
+                // Iterate until the current time reaches or exceeds the recent log timestamp
+                while (startTime <= recentLogTime)
+                {
+                    // Calculate the end time for the current time slot (3 hours later)
+                    var endTime = startTime.AddHours(3);
+
+                    // Format the time slot range
+                    var timeSlotRange = $"{startTime:dd-MM-yyyy HH:mm} - {endTime:dd-MM-yyyy HH:mm}";
+
+                    // Count the number of error logs within the time slot range
+                    var errorCount = logMessages.Count(logMessage => logMessage.TimeStamp >= startTime && logMessage.TimeStamp < endTime && (logMessage.Level == "Error" || logMessage.Level == "Critical"));
+
+                    // Add the time slot range and error count to the dictionary
+                    dict.Add(timeSlotRange, errorCount);
+
+                    // Update the start time for the next time slot
+                    startTime = endTime;
+                }
+
+                return Ok(dict);
+            }
+            catch (Exception e)
+            {
+                // Handle any exceptions
+            }
+
+            return Ok();
         }
 
         private string ExtractLogLevel(string logMessage)
         {
-            if (logMessage.Contains("Level:Information"))
+            if (logMessage.Contains("Level:Information") || logMessage.Contains("Level: Information"))
             {
                 return "Information";
             }
-            else if (logMessage.Contains("Level:Error"))
+            else if (logMessage.Contains("Level:Error") || logMessage.Contains("Level: Error"))
             {
                 return "Error";
             }
@@ -185,10 +383,11 @@ namespace LogFetcher.Controllers
         public DateTime TimeStamp { get; set; }
     }
 
-    public class LogDto
+    public class LogSearchRequest
     {
-        public string Level { get; set; }
-        public string Message { get; set; }
+        public string RegistrationId { get; set; }
+        public List<string> SelectedLevels { get; set; }
+        public List<string> SelectedIntervals { get; set; }
     }
 }
 
